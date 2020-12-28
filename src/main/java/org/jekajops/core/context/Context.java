1e8cb47a95e24f6db3341349bd856a9123ae6ac0
ВашеEvgenyKuzingit config --global user.email Ваш < evgenykuzin21@gmail.com>
2020-12-28 18:07:26 +0300

    clean garbage

--- src/main/java/org/jekajops/core/context/Context.java
+++ src/main/java/org/jekajops/core/context/Context.java
@@ -1,16 +1,8 @@
 package org.jekajops.core.context;
 
-import org.jekajops.core.utils.files.FileManager;
-
-import java.io.File;
 import java.sql.SQLException;
 
 public class Context {
-    volatile public static String ROOT = System.getProperty("user.dir");
-    volatile public static String RESOURCES = ROOT + "/src/main/resources/";
-    volatile public static File KEYBOARD_CONFIG_FILE = FileManager.getFileFromResources("config/keyboardConfig.txt");
-    volatile public static File SETTINGS_FILE = FileManager.getFileFromResources("properties/settings.properties");
-    volatile public static File AUDIO_LIST_FILE = FileManager.getFileFromResources("config/audioList.txt");
     volatile public static Settings SETTINGS;
     static {
         try {
